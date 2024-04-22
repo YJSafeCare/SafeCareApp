7dd0819c332bf9c28300d084b00fec4dd21772d8
plxs1257 <inix1257@gmail.com>
2024-04-22 15:26:21 +0900

    장소 추가 페이지 구현

---
+++ lib/Location/LocationAddPage.dart
@@ -0,0 +1,155 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:math' as Math;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../Data/LocationData.dart';
+
+class LocationAddPage extends StatefulWidget {
+  const LocationAddPage({super.key});
+
+  @override
+  _LocationAddPageState createState() => _LocationAddPageState();
+}
+
+class _LocationAddPageState extends State<LocationAddPage> {
+  late GoogleMapController mapController;
+  double _radius = 1.0;
+  final _locationNameController = TextEditingController();
+  LatLng _center = LatLng(35.8958520, 128.6218865);
+  Set<Circle> _circles = Set.from([
+    Circle(
+      circleId: CircleId('previewCircle'),
+      center: LatLng(35.8958520, 128.6218865),
+      radius: 1.0,
+      fillColor: Colors.blue.withOpacity(0.3),
+      strokeColor: Colors.blue,
+      strokeWidth: 1,
+    ),
+  ]);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  double calculateZoomLevel(double radius) {
+    double scale = radius / 300;
+    double zoomLevel = 16 - Math.log(scale) / Math.log(2);
+    return zoomLevel;
+  }
+
+  Future<void> sendDataToServer(LocationData data) async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:3001/locations'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data.toJson()),
+      );
+
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        log('Server responded with status code: ${response.statusCode}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('장소가 추가되었습니다.'),
+          ),
+        );
+        Navigator.pop(context);
+      }
+    } on SocketException catch (e) {
+      print('Failed to send data to server: $e');
+    } catch (e) {
+      print('An unexpected error occurred: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('장소 추가'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _locationNameController,
+            decoration: InputDecoration(
+              labelText: '장소명',
+            ),
+          ),
+          Slider(
+            value: _radius,
+            min: 1,
+            max: 1000,
+            divisions: 1000 - 1,
+            label: '반경 $_radius m',
+            onChanged: (double value) {
+              setState(() {
+                _radius = value;
+                double zoomLevel = calculateZoomLevel(_radius);
+                mapController.animateCamera(
+                  CameraUpdate.zoomTo(zoomLevel),
+                );
+              });
+            },
+          ),
+          Text('Current radius: $_radius'), // This line shows the current radius
+          Expanded(
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: _center,
+                      zoom: 16.0 - _radius / 100,
+                    ),
+                    onCameraMove: (CameraPosition position) {
+                      setState(() {
+                        _center = position.target;
+                        _circles = Set.from([
+                          Circle(
+                            circleId: CircleId('previewCircle'),
+                            center: _center,
+                            radius: _radius,
+                            fillColor: Colors.blue.withOpacity(0.3),
+                            strokeColor: Colors.blue,
+                            strokeWidth: 1,
+                          ),
+                        ]);
+                      });
+                    },
+                    circles: _circles,
+                    zoomControlsEnabled: false,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      var data = LocationData(
+                        locationName: _locationNameController.text,
+                        center: _center,
+                        radius: _radius,
+                      );
+                      sendDataToServer(data);
+                    },
+                    child: Text('생성'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
