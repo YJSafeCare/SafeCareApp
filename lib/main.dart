c7df45150917215e33532d3146090fe4f459b008
plxs1257 <inix1257@gmail.com>
2024-05-11 17:12:44 +0900

    FCM 관련 셋업

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,62 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:safecare_app/Alarm/FirebaseMessagingHandler.dart';
 
 import 'Login/LoginPage.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('Handling a background message ${message.messageId}');
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your channel id', 'your channel name',
+      importance: Importance.max, priority: Priority.high, showWhen: false);
+  var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics);
+  await flutterLocalNotificationsPlugin.show(
+      0, 'New Message', 'You have a new message', platformChannelSpecifics,
+      payload: message.data['type']);
+
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  var initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  FirebaseMessagingHandler firebaseMessagingHandler = FirebaseMessagingHandler();
+  await firebaseMessagingHandler.initializeFirebaseMessaging();
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  runApp(MyApp(firebaseMessagingHandler: firebaseMessagingHandler));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FirebaseMessagingHandler firebaseMessagingHandler;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.firebaseMessagingHandler});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SafeCare',
+      navigatorKey: firebaseMessagingHandler.navigatorKey,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: LoginPage(),
     );
   }
