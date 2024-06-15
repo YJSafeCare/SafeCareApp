a538567dee96191c9eb618d4a15b8c160a8d2f5f
plxs1257 <plxs1257@gmail.com>
2024-06-15 17:38:22 +0900

    로그인 정보가 실제로 상태관리 가능하게 변경, 단 OAuth는 미구현

--- lib/Login/LoginPage.dart
+++ lib/Login/LoginPage.dart
@@ -2,19 +2,22 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_provider/flutter_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:safecare_app/Map/MainMapPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../Data/User.dart';
 import '../Data/UserModel.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   // Define a controller for each text field
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    // UserModel userModel = Provider.of<UserModel>(context);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final username = ref.watch(userModelProvider);
     return Scaffold(
       appBar: AppBar(
         title: Text('Login Page'),
@@ -38,12 +41,16 @@ class LoginPage extends StatelessWidget {
               obscureText: true, // 비밀번호를 가리기 위해 사용
             ),
             ElevatedButton(
-              onPressed: () {
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-                User? currentUser = login(username, password, context);
-                if (currentUser != null) {
-                  // userModel.setUser(currentUser);
+              onPressed: () async {
+
+                String _username = _usernameController.text;
+                String _password = _passwordController.text;
+
+                ref.read(userModelProvider.notifier).username = _username;
+                ref.read(userModelProvider.notifier).userToken = "eyJhbGciOiJIUzI1NiJ9.eyJzZXJpYWwiOiIxMTIwMzE5ODkwNzkyMzE2NTQxNTAiLCJyb2xlIjoiVVNFUiIsImNhdGVnb3J5IjoiYWNjZXNzIiwibmFtZSI6ImluaXgiLCJwcm92aWRlciI6Imdvb2dsZSIsImlhdCI6MTcxODQzOTQyOCwiZXhwIjoxNzIwMjM5NDI4fQ.KI7tFTIOJ1uN45wxCd0QcoM63j25tskqS400NLnBUXo";
+                // 실제로 Oauth2를 사용하여 로그인을 구현할 때는 토큰을 받아와야 함
+
+                if (true) {
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => MainPage()),
@@ -58,6 +65,12 @@ class LoginPage extends StatelessWidget {
               },
               child: Text('Login'),
             ),
+            ElevatedButton(
+              onPressed: () {
+                googleSignIn();
+              },
+              child: Text('Register'),
+            ),
           ],
         ),
       ),
@@ -65,13 +78,34 @@ class LoginPage extends StatelessWidget {
   }
 
   User? login(String username, String password, BuildContext context) {
-    // User user = User(id: "temp", name: username, phone: "phone", image: "http://via.placeholder.com/400x400");
-    // Provider.of<UserModel>(context, listen: false).setUser(user);
-    
+    User currentUser = User(
+        serial: "temp",
+        memberId: "temp",
+        email: "temp",
+        name: username,
+        phone: "phone",
+        image: "http://via.placeholder.com/400x400");
+
     // 일단은 유저네임이 비어있지 않다면 통과
     if (username.isEmpty) {
       return null;
     }
     // return user;
+  }
+
+  Future<void> googleSignIn() async {
+    // 고유한 redirect uri
+    const APP_REDIRECT_URI = "com.yju.safecare";
+
+    // 백엔드에서 미리 작성된 API 호출
+    final url = Uri.parse('http://10.0.2.2:8080/login/oauth2/code/google');
+
+    // 백엔드가 제공한 로그인 페이지에서 로그인 후 callback 데이터 반환
+    final result = await FlutterWebAuth.authenticate(
+        url: url.toString(), callbackUrlScheme: APP_REDIRECT_URI);
+
+    // 백엔드에서 redirect한 callback 데이터 파싱
+    final accessToken = Uri.parse(result).queryParameters['access-token'];
+    final refreshToken = Uri.parse(result).queryParameters['refresh-token'];
   }
 }
