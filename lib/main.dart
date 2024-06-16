636e70caa29989546a6ded688171f53bbefe8ba4
plxs1257 <plxs1257@gmail.com>
2024-06-16 20:37:19 +0900

    메인에서 그룹 fetch후 상태관리값에 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -5,8 +7,16 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:safecare_app/Alarm/FirebaseMessagingHandler.dart';
+import 'package:safecare_app/Map/MainMapPage.dart';
 
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+import 'Data/Group.dart';
+import 'Data/UserModel.dart';
 import 'Login/LoginPage.dart';
+import 'constants.dart';
 import 'firebase_options.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -28,6 +38,7 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    name: 'SafeCare',
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
@@ -51,23 +62,52 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   final FirebaseMessagingHandler firebaseMessagingHandler;
 
   const MyApp({super.key, required this.firebaseMessagingHandler});
 
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(userModelProvider.notifier).userSerial = '112031989079231654150';
+    // ref.read(userModelProvider.notifier).username = '유저명';
+    ref.read(userModelProvider.notifier).userToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzZXJpYWwiOiIxMTIwMzE5ODkwNzkyMzE2NTQxNTAiLCJyb2xlIjoiVVNFUiIsImNhdGVnb3J5IjoiYWNjZXNzIiwibmFtZSI6ImluaXgiLCJwcm92aWRlciI6Imdvb2dsZSIsImlhdCI6MTcxODUzNzMyMSwiZXhwIjoyMDAwMTcxODUzNzEyMX0.aG-nWQhStG0yCEfAXnuq1fIRF6-9YUwCMC2X2uruE48';
+
+    fetchGroups();
+  }
+
+  Future<void> fetchGroups() async {
+    final response = await http.get(
+        Uri.parse('${ApiConstants.API_URL}/api/groups'),
+        headers: <String, String>{
+          'Authorization': ref.read(userModelProvider.notifier).userToken,
+        });
+
+    if (response.statusCode == 200) {
+      String responseBody = convert.utf8.decode(response.bodyBytes);
+      List<Group> groups = jsonDecode(responseBody).map<Group>((group) => Group.fromJson(group)).toList();
+      ref.read(userModelProvider.notifier).userGroups = groups;
+      print(responseBody);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'SafeCare',
-        navigatorKey: firebaseMessagingHandler.navigatorKey,
+        navigatorKey: widget.firebaseMessagingHandler.navigatorKey,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: LoginPage(),
+        home: MainPage(),
       );
   }
 }
